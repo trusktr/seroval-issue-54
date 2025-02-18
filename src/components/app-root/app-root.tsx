69ad40be9283382b8bdbeac65042f549c1e030f8
Joe Pea <joe@trusktr.io>
2025-02-18 00:31:49 -0800

    trim the reproduction

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,7 +1,5 @@
 import { Component, h } from '@stencil/core';
 import '../../KitchenSink.js';
-import type { KitchenSink } from '../../KitchenSink.js';
-import { createEffect } from 'solid-js';
 
 @Component({
   tag: 'app-root',
@@ -9,53 +7,7 @@ import { createEffect } from 'solid-js';
   shadow: true,
 })
 export class AppRoot {
-  sink!: KitchenSink;
-  sink2!: KitchenSink;
-
-  componentDidLoad() {
-    createEffect(() => {
-      console.log('values outside the element:', this.sink.count, this.sink.name, this.sink.doingSomething);
-    });
-
-    // Event listeners can be set on 'on*' event properties directly, as with builtin events.
-    this.sink2.onawesomeness = event => {
-      console.log('more awesomeness happened!', event.type);
-    };
-  }
-
   render() {
-    return (
-      <div>
-        {/* Start with an initial value of 5 */}
-        <kitchen-sink
-          ref={e => (this.sink = e)}
-          id="sink"
-          count="5"
-          name="Mo"
-          doingSomething
-          onClick={() => {
-            this.sink.count++;
-            this.sink.name += 'Mo';
-            // Get or set attributes (dash-case)
-            this.sink.setAttribute(
-              'doing-something',
-              // Or get or set the same-name properties (camelCase)
-              this.sink.doingSomething ? 'false' : 'true',
-            );
-
-            console.log('doingSomething after attribute change:', this.sink.doingSomething);
-          }}
-          onawesomeness={event => {
-            // The 'on*' event properties are also available in JSX or `html` templates.
-            console.log('awesomeness happened!', event.type);
-          }}
-        ></kitchen-sink>
-
-        <kitchen-sink ref={e => (this.sink2 = e)} id="sink2" count="1" name="Po" doingSomething="false" color="blue">
-          <p>child from light DOM, no slot specified</p>
-          <p slot="foo">child from light DOM, slotted to the foo slot</p>
-        </kitchen-sink>
-      </div>
-    );
+    return <kitchen-sink>foo</kitchen-sink>;
   }
 }
